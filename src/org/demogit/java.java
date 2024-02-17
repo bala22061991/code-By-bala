05f67d1e0e0f0554f4dda7ba7a4253d6120a197b
bala22061991 <vichu432@gmail.com>
2024-02-17 14:06:39 +0530

    update

--- src/org/demogit/java.java
+++ src/org/demogit/java.java
@@ -2,11 +2,18 @@ package org.demogit;
 
 public class java {
 
-	public static void main(String[] args) {
-		
-			System.out.println("method2");
-
+	private void addtocart() {
+		System.out.println("method1");
 
 	}
+	private void billing() {
+		System.out.println("method2");
 
+	}
+	public static void main(String[] args) {
+		java j = new java();
+		j.billing();
+		j.addtocart();
+
+}
 }
